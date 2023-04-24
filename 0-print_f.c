b28f0623617f13c07bc538e281b4bd42b1637d5e
SirBriann <bmaina049@yahoo.com>
2023-04-25 02:17:11 +0300

    0-print_f.c

--- 0-print_f.c
+++ 0-print_f.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 /**
  *_printf - function prototype
  * Description: produces output according to a format.
@@ -11,32 +12,31 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	const char *str;
-	char c;
 	int num = 0;
 
 	va_start(args, format);
 
 	while (*format)
+	{
 		if (*format == '%')
 		{
 			format++;
 
-			if (*format == 'c')
+			switch (*format)
 			{
-				c = va_arg(args, int);
-				putchar(c);
-				num++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(args, const char *);
-				num += printf("%s", str);
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				num++;
+				case 'c':
+					putchar(va_arg(args, int));
+					num++;
+					break;
+				case 's':
+					num += printf("%s", va_arg(args, char *));
+					break;
+				case '%':
+					putchar('%');
+					num++;
+					break;
+				default:
+					break;
 			}
 		}
 		else
@@ -44,7 +44,8 @@ int _printf(const char *format, ...)
 			putchar(*format);
 			num++;
 		}
-
+		format++;
+	}
 	va_end(args);
 
 	return (num);
