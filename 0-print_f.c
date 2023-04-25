d82751196bc6621fbc897fd5f6718ed532b5b78a
thinktank01 <felixayot@gmail.com>
2023-04-25 17:17:11 +0300

    Modify 0-print_f.c

--- 0-print_f.c
+++ 0-print_f.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- *_printf - function prototype(produces output according to a format.)
+ *_printf - function prototype
+ * Description: produces output according to a format.
  * @format: last argument
  * Return: num
  */
@@ -10,30 +11,19 @@ int _printf(const char *format, ...)
 	int num = 0;
 
 	if (!format)
-	return (-1);
+		return (-1);
 	va_start(args, format);
 	while (*format)
-		{
+	{
 		*format == '%' ? (
-			*++format == '\0' ? (
-				va_end(args), -1
-			) : (
-				*format == 'c' ? (
-					num++, putchar(va_arg(args, int))
-				) : *format == 's' ? (
-					num += printf("%s", va_arg(args, char *))
-				) : *format == '%' ? (
-					num++, putchar('%')
-				) : *format == 'd' || *format == 'i' ? (
-					num += printf("%d", va_arg(args, int))
-				) : (
-					num += printf("%c", *format), num++
-				)
-			)
-		) : (
-			num++, putchar(*format)
-		);
-		format++;
+			*++format == '\0' ? (va_end(args), -1) :
+			*format == 'c' ? (num++, putchar(va_arg(args, int))) :
+			*format == 's' ? (num += printf("%s", va_arg(args, char *))) :
+			*format == '%' ? (num++, putchar('%')) :
+			*format == 'd' || *format == 'i' ?
+			(num += printf("%d", va_arg(args, int))) :
+			(putchar('%'), putchar(*format), num += 2), 0
+		) : (putchar(*format), num++, 0), format++;
 	}
 	va_end(args);
 	return (num);
