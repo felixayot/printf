c862839d4c3836ccb875ef4e487a2298690e0d3b
SirBriann <bmaina049@yahoo.com>
2023-04-26 00:26:51 +0300

    0-print_f.c

--- 0-print_f.c
+++ 0-print_f.c
@@ -38,20 +38,23 @@ int _printf(const char *format, ...)
 					num += printf("%d", va_arg(args, int));
 					break;
 				case 'b':
-					num += printf("%u", va_arg(args, size_t));
+					num += printf("%b", va_arg(args, size_t));
 					break;
 				case 'u':
 					num += printf("%u", va_arg(args, size_t));
 					break;
 				case 'o':
-					num += printf("%u", va_arg(args, size_t));
+					num += printf("%o", va_arg(args, size_t));
 					break;
 				case 'x':
-					num += printf("%u", va_arg(args, size_t));
+					num += printf("%x", va_arg(args, size_t));
 					break;
 				case 'X':
-					num += printf("%u", va_arg(args, size_t));
+					num += printf("%X", va_arg(args, size_t));
 					break;
+				case 'p':
+					num += printf("%p", va_arg(args, size_t));
+					break'
 				default:
 					putchar('%');
 					putchar(*format);
