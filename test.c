7a54294bcb261788013e2c6e7b616a16ad72157c
SirBriann <bmaina049@yahoo.com>
2023-04-25 23:41:47 +0300

    check

--- test.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-int main()
-{
-	int count = _printf("Hello %s! Your favorite character is %c. Your average in %% is 140.\n", "Bro", 'S');
-	printf("Number of characters printed: %d\n", count);
-	return (0);
-}
